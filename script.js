d35b60246c8bcec471a84c7278c0135b3913db29
raisaaax <silearaisa@gmail.com>
2024-10-15 18:35:55 +0100

    adding first lesson preview

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let webstore = new Vue({
     el: "#app",
     data:{
+        showProduct: true,
         pageTitle:"After School Classes", 
         pageContent:"Afterschool classes can be booked here",
         product1: { 
@@ -8,17 +9,41 @@ let webstore = new Vue({
             title: "Maths Catch-up",
             location: "London",
             price: "£100",
-            availableSpaces: 5,
-            image: "/images/maths.jpg"
+            availSp: 5,
+            image: "images/maths.jpg"
             },
         product2: { 
-            id:1001,
+            id:1002,
             title: "English Catch-up",
             location: "London",
             price: "£80",
-            availableSpaces: 5,
-            image: "/images/english.png"
+            availSp: 5,
+            image: "images/english.png"
             },
         cart: []
+    },
+
+    methods:{
+
+        showCheckout(){
+            this.showProduct = this.showProduct? false: true;
+        },
+
+        addItem(){
+            this.cart.push(this.product1.id);
+            console.log(this.cart);
+        }
+
+    }, 
+
+    computed:{
+        cartItemCount(){
+            return this.cart.length || "";
+
+        },
+        canAddToCart(){
+            return this.product1.availSp > this.cartItemCount;
+        }
     }
+
 })
