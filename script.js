272e268fc3496be011fe58fa1edf204e7f1a6470
raisaaax <silearaisa@gmail.com>
2024-11-25 22:30:46 +0000

    fetching products from db

--- script.js
+++ script.js
@@ -1,113 +1,10 @@
-let products = [
-    { 
-        id:1001,
-        title: "Maths Catch-up",
-        location: "Chelsea",
-        price: "£50",
-        availSp: 10,
-        image: "images/maths.jpg"
-    },
-
-    { 
-        id:1002,
-        title: "English Catch-up",
-        location: "Westminster",
-        price: "£50",
-        availSp: 10,
-        image: "images/english.png"
-    },
-
-    { 
-        id:1003,
-        title: "History Catch-up",
-        location: "Westminster",
-        price: "£40",
-        availSp: 10,
-        image: "images/english.png"
-    },
-
-    { 
-        id:1004,
-        title: "Geography Catch-up",
-        location: "Richmond",
-        price: "£40",
-        availSp: 10,
-        image: "images/english.png"
-    },
-    { 
-        id:1005,
-        title: "Arts and Crafts Workshop",
-        location: "Islington",
-        price: "£55",
-        availSp: 10,
-        image: "images/art.png"
-    },
-    { 
-        id:1006,
-        title: "Advanced Maths",
-        location: "Chelsea",
-        price: "£80",
-        availSp: 10,
-        image: "images/maths.jpg"
-    },
-    { 
-        id:1007,
-        title: "Spanish Practice",
-        location: "Fulham",
-        price: "£60",
-        availSp: 10,
-        image: "images/spanish.jpg"
-    },
-    { 
-        id:1008,
-        title: "French Practice",
-        location: "Fulham",
-        price: "£60",
-        availSp: 10,
-        image: "images/english.png"
-    },
-    { 
-        id:1009,
-        title: "Netball Games",
-        location: "Ealing",
-        price: "£30",
-        availSp: 20,
-        image: "images/english.png"
-    },
-    { 
-        id:1010,
-        title: "Music Practice",
-        location: "Ealing",
-        price: "£70",
-        availSp: 10,
-        image: "images/english.png"
-    },
-    { 
-        id:1011,
-        title: "Cooking Class",
-        location: "Greenwich",
-        price: "£45",
-        availSp: 6,
-        image: "images/english.png"
-    },
-    { 
-        id:1012,
-        title: "Football Games",
-        location: "Ealing",
-        price: "£30",
-        availSp: 20,
-        image: "images/english.png"
-    },
-]
-
-
 let webstore = new Vue({
     el: "#app",
     data:{
         showProduct: true,
         pageTitle:"After School Classes", 
         pageContent:"Afterschool classes can be booked here",
-        products: products,
+        products: [],
         cart: [], 
         order: {
             firstName:"", 
@@ -121,8 +18,8 @@ let webstore = new Vue({
 
     created: {
 
-        fetchApp(){
-            fetch("https://cst3144cwapp-env.eba-3mniueut.eu-west-2.elasticbeanstalk.com").then(
+        function(){
+            fetch("https://cst3144cwapp-env.eba-3mniueut.eu-west-2.elasticbeanstalk.com/collections/products").then(
                 function (response) {
                     response.json().then(
                         function (json) {
